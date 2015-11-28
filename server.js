b5b984ca124dcd8ac0b0acd9ffaee0596b362daa
Tomoya Suzuki <suzutomo@toralab.org>
2015-11-28 15:06:15 +0900

    res send url

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.post('/grade', function(req, res){
     console.log(req.body);
     res.status(200);
+    res.send(req.body.url);
     res.end();
 });
 
