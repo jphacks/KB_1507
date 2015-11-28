7dd58cc93837b559bc8391b83d3f6f070d061957
Tomoya Suzuki <suzutomo@toralab.org>
2015-11-28 11:37:35 +0900

    use bodyparser

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+
+app.post('/result', function(req, res){
+    console.log(req.body);
+});
 
 app.listen(58000);
 console.log('server is running at localhost:58000');
