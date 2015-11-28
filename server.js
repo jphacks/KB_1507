1fc29b73c90cff42a3cc574270ccf7540b07723f
Tomoya Suzuki <suzutomo@toralab.org>
2015-11-28 16:15:27 +0900

    get_html

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
+var fs = require('fs');
 
 var app = express();
 
@@ -9,6 +11,14 @@ app.use(express.static(__dirname + '/public'));
 app.post('/grade', function(req, res){
     console.log(req.body);
     res.status(200);
+    request.get(req.body, function(err, res, body){
+        console.log(body);
+        fs.writeFile('./getData.html', body, function(err){
+            if(err){
+                console.error(err);
+            }
+        });
+    });
     res.send(req.body.url);
     res.end();
 });
